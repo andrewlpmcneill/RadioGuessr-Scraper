44054cdf2e71b91d81555e575499ac6349a3e936
Andrew McNeill <andrew@andrewmc.ca>
2022-03-17 00:05:34 -0300

    successfully retrieved some records

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const addStation = async() => {
   
     await axios.get('http://radio.garden/api/ara/content/places')
       .then(response => {
-        response.data.data.list.slice(0, 1).forEach(place => {
+        response.data.data.list.forEach(place => {
           const placeData = `(${place.id}, ${place.title}, ${place.country}, ${place.geo[0]}, ${place.geo[1]},`;
           axios.get(`http://radio.garden/api/ara/content/page/${place.id}/channels`)
             .then(response => {
@@ -18,7 +18,7 @@ const addStation = async() => {
                 const link = `http://radio.garden/api/ara/content/listen/${channelID}/channel.mp3`;
                 const stationData = ` ${channelID}, ${title}, ${link}),`;
                 const toWrite = placeData + stationData;
-                fs.writeFile('seed.sql', toWrite, { flag: 'a' });
+                fs.writeFile('seed2.sql', toWrite, { flag: 'a' });
               });
             });
         });
