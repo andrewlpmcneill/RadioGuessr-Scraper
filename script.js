63a78c9d0de450e1af22f9374f2c1d8c33fb7a86
Andrew McNeill <andrew@andrewmc.ca>
2022-03-17 11:01:41 -0300

    final seed?

--- script.js
+++ script.js
@@ -7,25 +7,30 @@ const addStation = async() => {
   
     await axios.get('http://radio.garden/api/ara/content/places')
       .then(response => {
-        response.data.data.list.slice(4000, 4002).forEach(place => {
-          const placeData = `(${place.id}, ${place.title}, ${place.country}, ${place.geo[0]}, ${place.geo[1]},`;
+        response.data.data.list.slice(0, 4700).forEach(place => {
+          let placeTitle = place.title;
+          if (placeTitle.includes("'")) placeTitle = placeTitle.replace("'", "''");
+          let placeCountry = place.country;
+          if (placeCountry.includes("'")) placeCountry = placeCountry.replace("'", "''");
+          const placeData = ` '${place.id}', '${placeTitle}', '${placeCountry}', ${place.geo[0]}, ${place.geo[1]}),\n`;
           axios.get(`http://radio.garden/api/ara/content/page/${place.id}/channels`)
             .then(response => {
               response.data.data.content[0].items.forEach(item => {
                 const split = item.href.split('/');
                 const channelID = split[split.length - 1];
-                const title = item.title;
+                let stationTitle = item.title;
+                if (stationTitle.includes("'")) stationTitle = stationTitle.replace("'", "''");
                 const link = `http://radio.garden/api/ara/content/listen/${channelID}/channel.mp3`;
-                const stationData = ` ${channelID}, ${title}, ${link}),\n`;
-                const toWrite = placeData + stationData;
-                fs.writeFile('seed3.sql', toWrite, { flag: 'a' });
+                const stationData = `('${channelID}', '${stationTitle}', '${link}',`;
+                const toWrite = stationData + placeData;
+                fs.writeFile('seed5.sql', toWrite, { flag: 'a' });
               });
             });
         });
       });
   
   } catch (error) {
-    console.error(`Got an error trying to write to a file: ${error.message}`);
+    console.error(`Got an error trying to write to a file.`);
   }
 
 };
